63f5aadc0954a2bd87fce527fa4796db1ca338d2
natasha <natashaputrir@student.amikom.ac.id>
2026-01-02 12:57:08 +0700

    update

--- api/index.php
+++ api/index.php
@@ -1,4 +1,17 @@
 <?php
+<?php
 
-// Forward Vercel requests to normal index.php
-require __DIR__ . '/../public/index.php';
+// Vercel serverless function for Laravel
+require __DIR__ . '/../vendor/autoload.php';
+
+$app = require_once __DIR__ . '/../bootstrap/app.php';
+
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+
+$response = $kernel->handle(
+    $request = Illuminate\Http\Request::capture()
+);
+
+$response->send();
+
+$kernel->terminate($request, $response);
